58e6000cf6b466517fb31412baf9f4cd134f0761
Sergey Amozov <sergey_amozov@belkatechnologies.com>
2021-01-15 21:27:06 +0200

    create level and player

---
+++ Assets/Scripts/General/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace General
+{
+    public class Player : MonoBehaviour
+    {
+        [SerializeField] private float _speed = 3f;
+
+        private Rigidbody _rigidbody;
+
+        private void Start()
+        {
+            _rigidbody = GetComponent<Rigidbody>();
+        }
+
+        protected void Move()
+        {
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            float moveVertical = Input.GetAxis("Vertical");
+
+            Vector3 movement = new Vector3(moveHorizontal, 0f, moveVertical);
+
+            _rigidbody.AddForce(movement * _speed);
+        }
+    }
+}
---
+++ Assets/Scripts/General/PlayerBall.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace General
+{
+    public class PlayerBall: Player
+    {
+        private void FixedUpdate()
+        {
+            Move();
+        }
+    }
+}
